8befef3afa71a568e470fc69e0f180f52addf6d9
sun7ren <155711234+sun7ren@users.noreply.github.com>
2025-10-28 17:15:46 +0700

    book queue simple logic

--- main.go
+++ main.go
@@ -3,6 +3,33 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Welcome to our photobooth")
+	boothName := "Thematics Booth"
+	const maxQueue = 50
+	var remainingQueue = 50
+
+	fmt.Printf("Welcome to %v photobooth", boothName)
+	fmt.Printf("We have a total of %v tickets, and a remaining of %v tickets", maxQueue, remainingQueue)
 	fmt.Println("Get your ticket queue here to start")
+
+	var firstName string
+	var email string
+	var amountOfTickets int
+
+	fmt.Println("Enter your first name: ")
+	fmt.Scan(&firstName)
+
+	fmt.Println("Enter your email: ")
+	fmt.Scan(&email)
+
+	fmt.Println("Enter the amount of Tickets: ")
+	fmt.Scan(&amountOfTickets)
+
+	fmt.Printf("User %v booked %v queue tickets.\n", firstName, amountOfTickets)
+
+	if remainingQueue >= amountOfTickets {
+		remainingQueue = remainingQueue - amountOfTickets
+		fmt.Printf("There are %v remaining queue tickets", remainingQueue)
+	} else {
+		fmt.Printf("Failed transaction.")
+	}
 }
